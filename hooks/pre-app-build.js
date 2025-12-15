5202b6ac45b9a6b519a1307dab9fb869cb7cbc02
Federico Soich <federico.soich@fulcrumdigital.com>
2025-12-15 12:45:31 -0300

    re-add info action

--- hooks/pre-app-build.js
+++ hooks/pre-app-build.js
@@ -10,17 +10,33 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-const fs = require('fs');
-const path = require('path');
+#!/usr/bin/env node
+
+/*
+Copyright 2025 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
 const { Core } = require('@adobe/aio-sdk');
+const { main: syncOAuthCredentials } = require('../scripts/sync-oauth-credentials');
+
 const logger = Core.Logger('hooks/pre-app-build', { level: process.env.LOG_LEVEL || 'info' });
 
-module.exports = () => {
-  const syncPath = path.join(__dirname, '..', 'scripts', 'sync-oauth-credentials.js');
-  if (fs.existsSync(syncPath)) {
-    require(syncPath).main();
-    logger.info('Synced OAuth credentials');
-  } else {
-    logger.info('Skipping sync-oauth-credentials (script not present)');
-  }
+const hook = async () => {
+  await syncOAuthCredentials();
+  logger.info('Done');
 };
+
+// Run if executed directly (as a script)
+if (require.main === module) {
+  hook();
+}
+
+module.exports = hook;
--- src/commerce-backend-ui-1/hooks/pre-app-build.js
+++ src/commerce-backend-ui-1/hooks/pre-app-build.js
@@ -1,2 +1,2 @@
 // Proxy to root hook so both extension-relative and project-root relative paths work.
-module.exports = require('../../hooks/pre-app-build.js');
+module.exports = require('../../../hooks/pre-app-build.js');
